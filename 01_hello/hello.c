00faa0be0bf277efbe16a78bacd1d25110179494
Prathamesh Rahate <patyarahate@gmail.com>
2025-04-15 07:10:45 +0530

    basic hello module

---
+++ 01_hello/hello.c
@@ -0,0 +1,20 @@
+#include <linux/module.h>
+#include <linux/init.h>
+
+
+int my_init(void)
+{
+    printk("Hello Kernel!\n");
+    return 0;
+}
+
+void my_exit(void)
+{
+    printk("GoodBye Kernel !!!\n");
+}
+
+module_init(my_init);
+module_exit(my_exit);
+
+
+MODULE_LICENSE("GPL");
